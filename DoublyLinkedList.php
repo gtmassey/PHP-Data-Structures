384a7ad6f165f920c2a616a91537498f4ba1c9e3
Garrett Massey <garrettmassey@unm.edu>
2022-10-12 10:00:23 -0600

    Initial Commit

---
+++ DoublyLinkedList.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * DoublyLinkedList
+ */
+class DoublyLinkedList {
+
+	public $head;
+
+	/**
+	 * LinkedList default constructor
+	 *
+	 * @param $data
+	 */
+	public function __construct()
+	{
+		$this->head = null;
+	}
+
+	/**
+	 * Add a new node with $data to the end of the list
+	 *
+	 * @param $data
+	 */
+	public function add($data)
+	{
+		$node = new Node($data);
+		if($this->head == null) {
+			$this->head = $node;
+		} else {
+			$current = $this->head;
+			while($current->next != null) {
+				$current = $current->next;
+			}
+			$current->next = $node;
+		}
+	}
+
+	public function addAt($position, $data) {
+		$node = new Node($data);
+		$current = $this->head;
+		$previous = null;
+		$i = 0;
+		if($position == 0) {
+			$node->next = $this->head;
+			$this->head = $node;
+		} else {
+			while ($i < $position) {
+				$i++;
+				$previous = $current;
+				$current = $current->next;
+			}
+			$node->next = $current;
+			$previous->next = $node;
+		}
+	}
+
+	public function addFirst($data) {
+		$node = new Node($data);
+		if($this->head != null) {
+			$node->next = $this->head;
+		}
+		$this->head = $node;
+	}
+
+	public function remove($data)
+	{
+		$current = $this->head;
+		$previous = null;
+		while($current != null) {
+			if($current->data == $data) {
+				if($previous == null) {
+					$this->head = $current->next;
+				} else {
+					$previous->next = $current->next;
+				}
+				return;
+			}
+			$previous = $current;
+			$current = $current->next;
+		}
+	}
+
+	public function removeAt($position)
+	{
+		$current = $this->head;
+		$previous = null;
+		$i = 0;
+		while($current != null) {
+			if($i == $position) {
+				if($previous == null) {
+					$this->head = $current->next;
+				} else {
+					$previous->next = $current->next;
+				}
+				return;
+			}
+			$previous = $current;
+			$current = $current->next;
+			$i++;
+		}
+	}
+
+	public function length()
+	{
+		$current = $this->head;
+		$length = 0;
+		while($current != null) {
+			$length++;
+			$current = $current->next;
+		}
+		return $length;
+	}
+
+	public function get()
+	{
+		return $this->head;
+	}
+
+	public function getAt($position)
+	{
+		$current = $this->head;
+		$i = 0;
+		while($current != null) {
+			if($i == $position) {
+				return $current;
+			}
+			$i++;
+			$current = $current->next;
+		}
+		return null;
+	}
+
+	public function toArray()
+	{
+		$array = [];
+		$current = $this->head;
+		while($current != null) {
+			$array[] = $current->data;
+			$current = $current->next;
+		}
+		return $array;
+	}
+
+}
---
+++ Node.php
@@ -0,0 +1,29 @@
+<?php
+
+class Node {
+	public $data;
+	public $next;
+	public $prev;
+
+	public function __construct($data) {
+		$this->data = $data;
+		$this->next = null;
+		$this->prev = null;
+	}
+
+	public function data() {
+		return $this->data;
+	}
+
+	public function next() {
+		return $this->next;
+	}
+
+	public function isEnd() {
+		return $this->next === null;
+	}
+
+	public function isStart() {
+		return $this->prev === null;
+	}
+}
